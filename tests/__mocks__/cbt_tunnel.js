137722d4291956229d4b1276625a0846541340de
Daphne Magsby <39204251+daphnemcrossbrowser@users.noreply.github.com>
2019-06-04 14:40:02 -0500

    Update cbt_tunnel.js

--- tests/__mocks__/cbt_tunnel.js
+++ tests/__mocks__/cbt_tunnel.js
@@ -1,12 +1,7 @@
-const Cbt = jest.genMockFromModule('cbt_tunnels')
+const cbtTunnels = jest.genMockFromModule('cbt_tunnels')
+const start = jest.fn().mockImplementation((options, cb) => cb(null, null))
+const stop = jest.fn().mockImplementation((cb) => cb(null))
 
-export const mockStart = jest.fn().mockImplementation((options, cb) => cb(null, null))
-export const mockStop = jest.fn().mockImplementation((cb) => cb(null))
-
-export const mockLocal = jest.fn().mockImplementation( function () {
-    this.start = mockStart
-    this.stop = mockStop
-})
-
-Cbt.Local = mockLocal
-module.exports = Cbt
+cbtTunnels.start = start
+cbtTunnels.stop = stop
+module.exports = cbtTunnels
